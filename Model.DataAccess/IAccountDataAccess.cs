d9218b9b147567fa4ec53c789a7e37893645ee60
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-05-25 15:52:30 +0200

    Groups the elements.

--- Model.DataAccess/IAccountDataAccess.cs
+++ Model.DataAccess/IAccountDataAccess.cs
@@ -5,14 +5,15 @@ namespace Model.DataAccess
 {
     public interface IAccountDataAccess
     {
+        Task<int> CreateAccount(LoginDTO accountInfo);
         AccountDTO GetAccount(int accountID);
         Task<LoginDTO> GetAccount(long PESEL);
         AccountInfoDTO GetAccountInfo(int accountID);
         byte[] GetAccountPassword(int accountID);
+        IEnumerable<BusinessDTO> GetComapnies();
         IEnumerable<InvoiceDetailsDTO> GetInvoiceDetails(int invoiceID);
         IEnumerable<InvoiceBusinessDTO> GetInvoicesInfo(int accountID, int categoryID);
         Task<bool> PayInvoice(int invoiceID, decimal value);
         Task<int> SetsNewPassword(int accountID, byte[] pass);
-        Task<int> CreateAccount(LoginDTO accountInfo);
     }
 }
